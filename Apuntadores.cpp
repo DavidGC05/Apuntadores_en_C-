d4b35536a9d083500cdeae8abb2e08ce797c8f05
DavidGC05 <davegrancast@gmail.com>
2024-10-29 21:46:38 -0600

    Add files via upload

---
+++ Apuntadores.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Apuntadores de memoria
+	int a = 10;
+	// Declaramos variable apuntador con:
+	int *b = &a;
+	
+	// Impresion de datos
+	cout << "Varable b(D. de memoria): " << b << endl;
+	cout << "Varable a: " << a << endl;
+	cout << "Apuntador *b: " << *b << endl;
+	cout << "D. de memoria de a: " << &a << endl;
+	// Modificacion de variable a con apuntador *b:
+	*b = 20;
+	cout << "Variable 'a' modificada con *b: " << a;
+	return 0;
+}
+
---
+++ Apuntadores_y_arreglos.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Arreglos y apuntadores
+	int largoArreglo;
+	
+	cout << "Introduce largo del arreglo: ";
+	cin >> largoArreglo;
+	
+	int *numeros = new int [largoArreglo];
+	
+	for(int i = 0; i < largoArreglo; i++){
+		cout << "Introduce el valor ["<< i << "]: " << endl;
+		cin >> numeros [i];
+	}
+	cout << endl<< "Valores Guardados:" << endl;
+	for(int i = 0; i < largoArreglo; i++){
+		cout << "Valor arreglo ["<< i << "]: " << numeros[i] << endl;
+	}
+	
+	delete [] numeros;
+	return 0;
+}
+
